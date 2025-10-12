e92beca06f1fef3aefad0968070625d06232031c
Alberto Olmos <albrtho94@gmail.com>
2025-10-12 12:25:47 -0600

    definiendo la ulr y el metodo ApiView

--- perfiles_api/views.py
+++ perfiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+  def get(self, request, format= None):
+    
+    caracteristicas = [
+      'Usa los metodos HTTP como una funcion get, post, patch, put, delete',
+      'Es similar al tradicional Django View',
+      'Da mayor control sobre la logica de tu aplicaciion',
+      'Is mapedo manualmente a las URLs'
+    ]
+    
+    return Response({'message': 'Hello world', 'caracteristicas':caracteristicas})
